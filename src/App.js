a5a27684f3562a02bd330ebbd926a1ec11b851f4
Jordan Ruetz <jordan.ruetz@mail.utoronto.ca>
2021-05-12 23:47:34 -0400

    created website 1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!!</h1>
+    <div id="root">
+      <div id="lines">
+        <h1>My name is Jordan.</h1>
+        <h1>I am a software developer.</h1>
+        <h1>I am also a student.</h1>
+        <h1>I interned at Flipp.</h1>
+        <h1>Then I interned at Amazon.</h1>
+        <h1>Then I interned at PointClickCare.</h1>
+        <h1>Then I interned at Kinaxis.</h1>
+        <h1>I like to code things.</h1>
+        <h1>Sometimes I go to hackathons. (8x)</h1>
+        <h1>Here is my <a href="./jordan-ruetz-resume.pdf"><span class="underline">resume</span></a>.</h1>
+        <h1>I am currently looking for fulltime jobs!</h1>
+      </div>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       {
         test: /\.(css|scss)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif|ttf)$/,
