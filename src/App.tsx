d134f8bc9dd2535a5de5d6e4cd43c621efa5ea4d
Jordan Ruetz <JordanR.9813@gmail.com>
2025-02-01 23:29:53 -0500

    replace project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Typography > My name is Jordan Ruetz.</Typography >
+      </div>
+    </>
+  )
+}
+
+export default App
